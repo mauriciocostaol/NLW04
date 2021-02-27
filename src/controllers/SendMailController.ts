3d0a07c3327aaa8c6256742b0fe645bbbde0c9a0
Mauricio Ricardo <mauricio.costa7@fatec.sp.gov.br>
2021-02-27 18:53:07 -0300

    codigo final api nps

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -5,6 +5,7 @@ import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
 import { UsersRepository } from "../repositories/UsersRepository";
 import SendMailService from "../services/SendMailService";
 import {resolve}  from "path";
+import { AppError } from "../errors/AppError";
 
 
 class SendMailController {
@@ -14,38 +15,37 @@ class SendMailController {
         const { email, survey_id } = request.body;
 
         const usersRepository = getCustomRepository(UsersRepository);
-        const surveysRespository = getCustomRepository(SurveysRepository);
+        const surveysRepository = getCustomRepository(SurveysRepository);
         const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
 
         const user = await usersRepository.findOne({ email });
 
         if (!user) {
-            return response.status(400).json({
-                error: "User does not exists",
-            });
+           throw new AppError("User does not exists");
         }
-        const survey = await surveysRespository.findOne({ id: survey_id });
+        const survey = await surveysRepository.findOne({ id: survey_id });
 
         if (!survey) {
-            return response.status(400).json({
-                error: "Survey does not exists",
-            });
+            throw new AppError("Survey does not exists");
         }
-        const variables ={
-            name: user.name,
-            title: survey.title,
-            description: survey.description,
-            user_id: user.id,
-            link: process.env.URL_MAIL,
-        }
+        
         const npsPath = resolve(__dirname,"..","views","emails","npsMail.hbs");
         const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
-            where:[{user_id: user.id},{value:null}],
+            where:{user_id: user.id,value:null},
             relations:["user","survey"],
 
         })
 
+        const variables ={
+            name: user.name,
+            title: survey.title,
+            description: survey.description,
+            id: "",
+            link: process.env.URL_MAIL,
+        }
+
         if(surveyUserAlreadyExists){
+            variables.id = surveyUserAlreadyExists.id;
             await SendMailService.execute(email,survey.title,variables, npsPath);
             return response.json(surveyUserAlreadyExists);
         }
@@ -54,7 +54,7 @@ class SendMailController {
             survey_id,
         });
         await surveysUsersRepository.save(surveyUser);
- 
+        variables.id = surveyUser.id;
        
         await SendMailService.execute(email,survey.title,variables,npsPath);
         return response.json(surveyUser);
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,23 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { UsersRepository } from '../repositories/UsersRepository';
+import * as yup from 'yup';
+import { AppError } from '../errors/AppError';
 
 class UserController {
     async create(request: Request, response: Response) {
         const {name,email} = request.body;
         
+        const schema = yup.object().shape({
+            name: yup.string().required(),
+            email: yup.string().email().required()
+        })
+
+      try{
+        await schema.validate(request.body,{abortEarly:false});
+      }catch(err){
+        throw new AppError(err);
+      }
         const usersRepository = getCustomRepository(UsersRepository);
         
         const userAlreadyExists = await usersRepository.findOne({
@@ -13,10 +25,7 @@ class UserController {
         });
 
         if(userAlreadyExists){
-            return response.status(400).json({
-                error:"User already exists!",
-                
-            });
+           throw new AppError("User already exists!");
         }
         const user = usersRepository.create({
             name,email
@@ -25,6 +34,13 @@ class UserController {
         
         return response.status(201).json(user);
         
+    }
+
+    async show (request: Request, response:Response){
+        const usersRepository = getCustomRepository(UsersRepository);
+        const all = await usersRepository.find();
+
+        return response.json(all);
     }
 }
 export { UserController };
